0cc20a929727f8e0e8638932eafe8617f033ab08
Thang Pham <inhousescript@gmail.com>
2021-02-25 12:21:59 +0100

    Cuzick test

--- ion.r
+++ ion.r
@@ -780,6 +780,38 @@ ion$beta_binomial_4g <- function(dat, group1, group2, group3, group4, total_coun
                  pval.BH = p.adjust(out$p.value, method = "BH")))
 }
 
+ion$cuzick_test <- function(x, z, test.type=c("two.sided", "upper", "lower")) {
+    
+    # https://r.789695.n4.nabble.com/Cuzick-s-test-for-trend-td807101.html
+    
+    N = length(z)
+    n = unique(z)
+    
+    ranks=rank(x)
+    
+    T = sum(ranks*z)
+    
+    p = (table(z)/N)
+    E_Z = sum(unique(z)*p)
+    E_T = 0.5*N*(N+1)*E_Z
+    
+    Var_Z = sum(unique(z)^2*p) - E_Z^2
+    Var_T = N^2*(N+1)/12*Var_Z
+    
+    Zscore = (T-E_T)/sqrt(Var_T)
+    
+    if(test.type == "two.sided") {
+        pval = 2*pnorm(-abs(Zscore))
+    } else if(test.type == "upper") {
+        pval = pnorm(Zscore,lower.tail=F)
+    } else pval = pnorm(Zscore,lower.tail=T)
+    
+    out = data.frame(cbind(Zscore,pval,test.type))
+    colnames(out) = c("Z","p","testType")
+    #return(out)
+    return(pval)
+}
+
 # Text processing ----
 
 ion$str_split <- function(comma_separated_text, sep = ",") {
